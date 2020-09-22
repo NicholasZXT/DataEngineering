4bd18ef0bce981b9d614f5478f7a01e7574ac69b
NicholasZXT <1036562425@qq.com>
2020-09-22 10:57:15 +0800

    leetcode-Q72

--- src/main/java/LeetCode/CommonTricks.java
+++ src/main/java/LeetCode/CommonTricks.java
@@ -10,7 +10,16 @@ public class CommonTricks {
 
         //1. Java数组建立之后，数组内的值默认初始化为0
         int[][] array = new int[3][3];
-        System.out.println(array[0][0]);
+//        System.out.println(array[0][0]);
+
+//        2. 如果要在for循环中多次修改一个String类型，应当使用StringBuilder类，这样不会产生一个新对象
+        StringBuilder sBuffer = new StringBuilder();
+        System.out.println(sBuffer);
+        sBuffer.append('a');
+        System.out.println(sBuffer);
+        sBuffer.append("bcd");
+        System.out.println(sBuffer);
+
 
     }
 }
