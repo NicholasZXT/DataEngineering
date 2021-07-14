7b8b17c19335d0e86e5fb05f350e3215043b0a09
NicholasZXT <1036562425@qq.com>
2021-07-14 13:45:51 +0800

    插入排序

--- src/main/java/SortAlgorithms/InsertingSort.java
+++ src/main/java/SortAlgorithms/InsertingSort.java
@@ -1,21 +1,40 @@
 package SortAlgorithms;
 
+import java.util.Arrays;
 
 /**
- * 简单插入排序的实现
+ * 简单插入排序
+ * 每次插入一个元素到末尾，检查此时是否有序，如果无序，就将新插入的元素逐个向前移动，直到序列重新有序
+ *
+ * 时间复杂度 ，空间复杂度
+ *
+ * 自己实现难度： easy
  */
 public class InsertingSort {
 
     public static void main(String[] args) {
+        Integer[] arr1 = {1, 8, 5, 7, 11, 3};
+        Integer[] arr2 = {};
+        Integer[] arr3 = {1};
+        InsertingSort.sort(arr1);
+        System.out.println(Arrays.toString(arr1));
+        InsertingSort.sort(arr2);
+        System.out.println(Arrays.toString(arr2));
+        InsertingSort.sort(arr3);
+        System.out.println(Arrays.toString(arr3));
 
     }
 
+    /**
+     * 插入排序的实现
+     * @param array
+     */
     public static void sort(Comparable[] array){
         int length = array.length;
         if (length < 2) return;
         for (int i = 1; i < length; i++){
             int cmp = array[i-1].compareTo(array[i]);
-            while ( cmp > 0){
+            while (cmp > 0){
                 exchange(array, i-1, i);
                 i--;
                 cmp = array[i-1].compareTo(array[i]);
