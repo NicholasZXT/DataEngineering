c2cc189d71d4da5e6159bec9add711f3ae677a2d
NicholasZXT <1036562425@qq.com>
2022-02-15 19:24:27 +0800

    算法题练习

---
+++ src/main/java/LeetCode/SwordOffer/Q6_ReconstructBinaryTree.java
@@ -0,0 +1,22 @@
+package LeetCode.SwordOffer;
+
+public class Q6_ReconstructBinaryTree {
+    public static void main(String[] args){
+
+    }
+
+    public BinaryTreeNode reconstruct(int[] preOrder, int[] midOrder){
+
+    }
+
+    private class BinaryTreeNode{
+        int value;
+        BinaryTreeNode left;
+        BinaryTreeNode right;
+        public BinaryTreeNode(int data, BinaryTreeNode l_child, BinaryTreeNode r_child){
+            value = data;
+            left = l_child;
+            right = r_child;
+        }
+    }
+}
