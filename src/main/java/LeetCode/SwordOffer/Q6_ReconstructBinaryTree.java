b1b083f33ca98fc158dd55f810ada2ae980ee97c
NicholasZXT <1036562425@qq.com>
2022-03-02 09:17:21 +0800

    算法练习

--- src/main/java/LeetCode/SwordOffer/Q6_ReconstructBinaryTree.java
+++ src/main/java/LeetCode/SwordOffer/Q6_ReconstructBinaryTree.java
@@ -1,22 +1,43 @@
 package LeetCode.SwordOffer;
 
+import scala.None;
+
 public class Q6_ReconstructBinaryTree {
     public static void main(String[] args){
+        BinaryTreeNode tree = new BinaryTreeNode(1);
+
 
     }
 
-    public BinaryTreeNode reconstruct(int[] preOrder, int[] midOrder){
+    //public BinaryTree reconstruct(int[] preOrder, int[] midOrder){
+    //
+    //
+    //}
 
-    }
-
-    private class BinaryTreeNode{
-        int value;
+    public class BinaryTreeNode{
+        int data;
         BinaryTreeNode left;
         BinaryTreeNode right;
-        public BinaryTreeNode(int data, BinaryTreeNode l_child, BinaryTreeNode r_child){
-            value = data;
-            left = l_child;
-            right = r_child;
+        public BinaryTreeNode(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+        public BinaryTreeNode(int data, BinaryTreeNode left, BinaryTreeNode right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+        public void midOrderShow(){
+            show(this);
+        }
+        private void show(BinaryTreeNode root){
+            if (root != null){
+                show(root.left);
+                System.out.print(root.data);
+                System.out.print(", ");
+                show(root.right);
+            }
         }
     }
 }
