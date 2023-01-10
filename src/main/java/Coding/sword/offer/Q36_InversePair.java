4179fa0d3b2a289ec2a21e915d936287a3a6d3c3
NicholasZXT <1036562425@qq.com>
2023-01-10 22:48:54 +0800

    sword-offer: Q36-todo

---
+++ src/main/java/Coding/sword/offer/Q36_InversePair.java
@@ -0,0 +1,12 @@
+package Coding.sword.offer;
+
+/**
+ * 在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个 逆序对.
+ * 输入一个数组，求出这个数组中的逆序对的总数。
+ * 比如在数组 {7,5,6,4} 中，一共存在 5 个逆序对：(7,6), (7,5), (7,4), (5,4), (6,4)
+ */
+public class Q36_InversePair {
+    public static void main(String[] args) {
+
+    }
+}
