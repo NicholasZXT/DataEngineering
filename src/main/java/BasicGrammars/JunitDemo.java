0daf6784fad7375290d043d4a4cd1a41c1bfd993
NicholasZXT <1036562425@qq.com>
2023-03-24 16:13:54 +0800

    Junit复习

--- src/main/java/BasicGrammars/JunitDemo.java
+++ src/main/java/BasicGrammars/JunitDemo.java
@@ -1,21 +1,34 @@
 package BasicGrammars;
 
+// 开发人员编写单元测试用的注解
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+// Junit 内部具体执行测试的接口
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
 public class JunitDemo {
 
-    public void demo1(){
-        System.out.println("demo1");
-    }
-
-    /*
-    正常情况下要运行上面的方法，需要在main函数中创建一个对象，通过对象调用方法
-     */
+    /*正常情况下要运行 JunitDemo 里的方法，需要在main函数中创建一个对象，通过对象调用方法*/
     public static void main(String[] args) {
+        // 手动测试流程
         JunitDemo junit = new JunitDemo();
         junit.demo1();
+
+        // Junit 框架的实际调用流程如下所示
+        // 不过在IDEA中使用时，为了能够实现可视化操作，IDEA还在Junit外封装了一层，主要是获取单元测试的信息，然后根据点击的按钮来执行具体测试
+        System.out.println("------- Junit runner --------");
+        Result result = JUnitCore.runClasses(JunitDemo.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        System.out.println(result.wasSuccessful());
+    }
+
+    public void demo1(){
+        System.out.println("demo1");
     }
 
     /*Junit使用
