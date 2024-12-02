b853978bb7720267b1eac41bc54d09ed9ac0a77d
NicholasZXT <1036562425@qq.com>
2024-12-02 22:34:35 +0800

    Flink算子状态练习

--- src/main/java/FlinkDemos/c11_state/OperatorStateDemos.java
+++ src/main/java/FlinkDemos/c11_state/OperatorStateDemos.java
@@ -1,20 +1,28 @@
 package FlinkDemos.c11_state;
 
+import org.apache.flink.api.common.state.BroadcastState;
+import org.apache.flink.api.common.state.ReadOnlyBroadcastState;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
-import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.BroadcastStream;
+import org.apache.flink.streaming.api.datastream.BroadcastConnectedStream;
+import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
-import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.runtime.state.FunctionInitializationContext;
-import org.apache.flink.runtime.state.FunctionSnapshotContext;
+import org.apache.flink.api.common.state.MapStateDescriptor;
 import org.apache.flink.api.common.state.ListState;
 import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.runtime.state.FunctionInitializationContext;
+import org.apache.flink.runtime.state.FunctionSnapshotContext;
 import java.time.Duration;
 
 import FlinkDemos.beans.WaterSensor;
 import FlinkDemos.c8_window.TimeWindowAgg;
+import org.apache.flink.util.Collector;
 
 /**
  * 演示 算子状态
@@ -44,8 +52,20 @@ public class OperatorStateDemos {
         // 在map算子中计算数据的个数
         sensorDS.map(new MyCountMapFunction()).print();
 
-        // TODO ---------------- BroadcastState 使用 ----------------
+        // ---------------- BroadcastState 使用 ----------------
         // 水位超过指定的阈值发送告警，阈值可以动态修改
+        // 配置流（用来广播配置）
+        DataStreamSource<String> configDS = env.socketTextStream("localhost", 7898);
+        // 1. 将 配置流 广播
+        MapStateDescriptor<String, Integer> broadcastMapState = new MapStateDescriptor<>(
+            "broadcast-state", Types.STRING, Types.INT
+        );
+        BroadcastStream<String> configBS = configDS.broadcast(broadcastMapState);
+        // 2.把 数据流 和 广播后的配置流 connect
+        BroadcastConnectedStream<WaterSensor, String> sensorBCS = sensorDS
+            .map(new TimeWindowAgg.WaterSensorMapFunction())
+            .connect(configBS);
+        sensorBCS.process(new MyBroadcastFunction(broadcastMapState)).print();
 
         env.execute();
     }
@@ -98,6 +118,52 @@ public class OperatorStateDemos {
                     count += c;
                 }
             }
+        }
+    }
+
+    public static class MyBroadcastFunction extends BroadcastProcessFunction<WaterSensor, String, String> {
+
+        private final MapStateDescriptor<String, Integer> broadcastMapState;
+
+        public MyBroadcastFunction(MapStateDescriptor<String, Integer> broadcastMapState){
+            this.broadcastMapState = broadcastMapState;
+        }
+        
+        /**
+         * 数据流的处理方法： 数据流 只能 读取 广播状态，不能修改
+         * @param value
+         * @param ctx
+         * @param out
+         * @throws Exception
+         */
+        @Override
+        public void processElement(
+            WaterSensor value, ReadOnlyContext ctx, Collector<String> out
+        ) throws Exception {
+            // 通过上下文获取广播状态，取出里面的值（只读，不能修改）
+            ReadOnlyBroadcastState<String, Integer> broadcastState = ctx.getBroadcastState(broadcastMapState);
+            Integer threshold = broadcastState.get("threshold");
+            // 判断广播状态里是否有数据，因为刚启动时，可能是数据流的第一条数据先来
+            threshold = (threshold == null ? 0 : threshold);
+            if (value.getVc() > threshold) {
+                out.collect(value + ",水位超过指定的阈值：" + threshold + "!!!");
+            }
+        }
+
+        /**
+         * 广播后的配置流的处理方法:  只有广播流才能修改 广播状态
+         * @param value
+         * @param ctx
+         * @param out
+         * @throws Exception
+         */
+        @Override
+        public void processBroadcastElement(
+            String value, Context ctx, Collector<String> out
+        ) throws Exception {
+            // 通过上下文获取广播状态，往里面写数据
+            BroadcastState<String, Integer> broadcastState = ctx.getBroadcastState(broadcastMapState);
+            broadcastState.put("threshold", Integer.valueOf(value));
         }
     }
 
